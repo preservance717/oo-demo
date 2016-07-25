8cf3e0efc46d52819da7db61b3d23af3c82dbe56
preservance717 <GL717@outlook.com>
2016-07-25 10:33:07 +0800

    modify

--- demo.js
+++ demo.js
@@ -4,11 +4,6 @@ class Animal {
     constructor() {
         this.type = 'animal';
     }
-
-    says(say) {
-        console.log(this.type + ' says ' + say);
-    }
 }
 
 let animal = new Animal();
-animal.says('Hello');
